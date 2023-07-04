5c08a16507b35b17f0830ae683a5bf990e36de2c
HaoyuScut <904264204@qq.com>
2023-07-04 21:05:38 +0800

    SpringDataRedis

--- src/main/java/com/itwhy/redis/pojo/User.java
+++ src/main/java/com/itwhy/redis/pojo/User.java
@@ -1,10 +1,19 @@
 package com.itwhy.redis.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @ClassName: {NAME}
  * @Auther: why
  * @Date: 2023/07/04 21 00
  * @Version: v1.0
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String name;
+    private Integer age;
 }
--- src/test/java/com/itwhy/RedisDemoApplicationTests.java
+++ src/test/java/com/itwhy/RedisDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.itwhy;
 
+import com.itwhy.redis.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +25,15 @@ class RedisDemoApplicationTests {
         //获取String数据
         Object name = redisTemplate.opsForValue().get("name");
         System.out.println(name);
+    }
+
+    @Test
+    void testSaveUser() {
+        //写入数据
+        redisTemplate.opsForValue().set("user:100", new User("虎哥", 21));
+        //获取数据
+        User o = (User) redisTemplate.opsForValue().get("user:100");
+        System.out.println("o = " + o);
     }
 
 }
